baeb44d99d9616ab67a715d889e5cff12f54a2bc
Wassim Tair <wassim@beytek.co>
2024-04-03 09:17:05 -0500

    implementing Breeze auth kit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\NewsletterController;
@@ -18,3 +19,15 @@ Route::get('/admin-panel/registrations', [AdminController::class, 'index']);
 Route::delete('/admin-panel/registrations/delete', [AdminController::class, 'deleteRegistration']);
 Route::get('/admin-panel/registrations/{id}', [AdminController::class, 'editRegistration']);
 Route::put('/admin-panel/registrations/{id}', [AdminController::class, 'updateRegistration']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';

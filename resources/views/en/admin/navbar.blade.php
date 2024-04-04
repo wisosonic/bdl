0304850c7b1ec879d91288493801bc02c30bbbfe
Wassim Tair <wassim@beytek.co>
2024-04-04 07:05:50 -0500

    adding logout button to navbar in admin panel

--- resources/views/en/admin/navbar.blade.php
+++ resources/views/en/admin/navbar.blade.php
@@ -5,7 +5,14 @@
             <a data-bs-toggle="modal" data-bs-target="#exampleModalToggle2" href="#" >Register Now</a>
         </li>
         @foreach ($alt_lang_tr as $index => $l)
-            <li><a href="?lang={{$alt_lang[$index]}}">{{$l}}</a></li>
+            <li>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <a href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
+                        {{ __('Log Out') }}
+                    </a>
+                </form>
+            </li>
         @endforeach
     </ul>
 </nav><!-- #nav-menu-container -->
